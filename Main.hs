9c242369e1460ac3adab6008ccd854efd5ad89d1
Tim Dysinger <tim@dysinger.net>
2013-05-20 07:23:56 -1000

    Refactor & clean up more

--- Main.hs
+++ Main.hs
@@ -33,18 +33,18 @@ import           System.FilePath
 import           System.IO
 
 -- | The Kommand data type contains the attributes of each Kommand.
-data Kommand = Kommand { _id          :: String
-                       , _aliases     :: Maybe [String]
-                       , _commands    :: Maybe [Kommand]
-                       , _description :: Maybe [String]
-                       , _examples    :: Maybe [String]
-                       , _path        :: Maybe FilePath
-                       , _synopsis    :: String
-                       , _url         :: Maybe String }
+data Kommand = K { _id          :: String
+                 , _aliases     :: Maybe [String]
+                 , _commands    :: Maybe [Kommand]
+                 , _description :: Maybe [String]
+                 , _examples    :: Maybe [String]
+                 , _path        :: Maybe FilePath
+                 , _synopsis    :: String
+                 , _url         :: Maybe String }
 
 -- | Instance of FromJSOM so we can map JSON records to Kommand.
 instance FromJSON Kommand where
-  parseJSON (Object v) = Kommand             <$>
+  parseJSON (Object v) = K                   <$>
                          v .:  "id"          <*>
                          v .:? "aliases"     <*>
                          v .:? "commands"    <*>
@@ -59,25 +59,27 @@ instance FromJSON Kommand where
 -- path isn't given then the kommand is a subcommand or flag.  In this
 -- case we print it's id.
 instance Show Kommand where
-  show (Kommand{_path=Nothing,_id=i}) = i
-  show (Kommand{_path=Just p}) = p
+  show (K{_path=Nothing,_id=i}) = i
+  show (K{_path=Just p}) = p
 
 matchKommand :: String -> Kommand -> Bool
-matchKommand x (Kommand{_aliases=Nothing,_id=i}) = i == x
-matchKommand x (Kommand{_aliases=Just xs,_id=i}) =
+matchKommand x (K{_aliases=Nothing,_id=i}) = i == x
+matchKommand x (K{_aliases=Just xs,_id=i}) =
   any ((==) (map toLower x)) (i:xs)
 
+-- | Produce a list of internal Kommands.  These are outside of the
+-- JSON spec & can be interposed with the tree of JSON Kommands.
 internalKommands :: [Kommand]
-internalKommands = [ Kommand { _id          = "help"
-                             , _aliases     = Just [ "-?", "-h", "--help" ]
-                             , _synopsis    = "Built in Help Kommand"
-                             , _commands    = Nothing
-                             , _description = Nothing
-                             , _examples    = Nothing
-                             , _path        = Nothing
-                             , _url         = Nothing } ]
+internalKommands = [ K { _id          = "help"
+                       , _aliases     = Just [ "-?", "-h", "--help" ]
+                       , _synopsis    = "Built in Help Kommand"
+                       , _commands    = Nothing
+                       , _description = Nothing
+                       , _examples    = Nothing
+                       , _path        = Nothing
+                       , _url         = Nothing } ]
 
--- | Lazily keep the kommands json file in sync with what we can find
+-- | Lazily keep the kommands JSON file in sync with what we can find
 -- on the internet.
 kommands :: IO (Either String [Kommand])
 kommands = do
@@ -91,12 +93,13 @@ kommands = do
               fetchKommands
   readKommands fname
 
--- | Calculate the location of our cached json file.
+-- | Calculate the location of our cached JSON file.
 filename :: IO FilePath
 filename = do
   home <- getHomeDirectory
   return $ home </> ".kommands.json"
 
+-- | Read the kommands file from disk & parse the JSON
 readKommands :: FilePath -> IO (Either String [Kommand])
 readKommands fname = BS.readFile fname >>= return . eitherDecode
 
@@ -114,58 +117,59 @@ data Stack = Stack [Kommand] [String] deriving Show
 -- that didn't match.
 initialStack :: Stack -> Stack
 initialStack (Stack ks as) = Stack found (reverse left)
-  where (_, found, left) = foldl reduceArgs (Just ks, [], []) as
+  where (_, (Stack found left)) = foldl reduceArgs (Just ks, (Stack [] [])) as
 
-reduceArgs :: (Maybe [Kommand], [Kommand], [String])
-           -> String
-           -> (Maybe [Kommand], [Kommand], [String])
-reduceArgs (x, ks, as) a =
+-- | Take a tuple of maybe a list of Kommands and a String arg.  If
+-- it's a match to any of the Kommands in our list then put it on the
+-- Kommand stack.  If not put it on the arg stack.
+reduceArgs :: (Maybe [Kommand], Stack) -> String -> (Maybe [Kommand], Stack)
+reduceArgs (x, Stack ks as) a =
   case (filter (matchKommand a) internalKommands) of
-    (k:_) -> (x, k:ks, as)
+    (k:_) -> (x, Stack (k:ks) as)
     _     -> case x of
-      Nothing -> (Nothing, ks, a:as)
+      Nothing -> (Nothing, Stack ks (a:as))
       Just y  -> case (filter (matchKommand a) y) of
-        (k:_) -> (_commands k, k:ks, as)
-        _     -> (Nothing, ks, a:as)
+        (k:_) -> (_commands k, Stack (k:ks) as)
+        _     -> (Nothing, Stack ks (a:as))
 
 -- | Take a Stack and build a new Stack by popping Kommands onto the
 -- args list until we can find a Kommand that has an executable path.
 exeStack :: Stack -> Stack
-exeStack (Stack [] _) = error "Kommand tree didn't have an executable path."
-exeStack s@(Stack (Kommand{_path=Just _}:_) _)      = s
-exeStack (Stack (k@(Kommand{_path=Nothing}):ks) as) =
+exeStack (Stack [] _) = error "Kommand didn't have an executable path."
+exeStack s@(Stack (K{_path=Just _}:_) _) = s
+exeStack (Stack (k@(K{_path=Nothing}):ks) as) =
   exeStack (Stack ks ((show k):as))
 
--- | Route transforms the initial stack from args into Kommands.  It
--- then routes the stack to the appropriate function.
-route :: Stack -> IO ()
-route (Stack [] []) = showKommands
-route (Stack ((Kommand{_id="help"}):[]) _)    = showKommands
-route (Stack ((Kommand{_id="help"}):k:ks) as) = showHelp (Stack (k:ks) as)
-route (Stack (k:(Kommand{_id="help"}):ks) as) = showHelp (Stack (k:ks) as)
-route s = putStrLn (show s) >> run s
-
+-- | Run analyzes the stack & routes it to the appropriate function.
 run :: Stack -> IO ()
-run (Stack [] [])     = mzero
-run (Stack [] (a:as)) = rawSystem a as          >>= exitWith
-run s                 = rawSystem (show k') as' >>= exitWith
-  where (Stack (k':_) as') = exeStack s
+run (Stack [] [])                     = showKommands
+run (Stack [] (a:as))                 = exitWith =<< rawSystem a as
+run (Stack ((K{_id="help"}):[]) _)    = showKommands
+run (Stack ((K{_id="help"}):ks) as)   = showHelp (Stack ks as)
+run (Stack (k:(K{_id="help"}):ks) as) = showHelp (Stack (k:ks) as)
+run s@(Stack (K{_path=Nothing}:[]) _) = showHelp s
+run s@(Stack (K{_path=Nothing}:_) _)  = run (exeStack s)
+run (Stack (k@K{_path=Just _}:_) as)  = exitWith =<< rawSystem (show k) as
 
+-- | Show the list of Kommands & their synopsis - 1 per line
 showKommands :: IO ()
 showKommands = putStrLn "OHAI!" -- TODO list all Kommands + Synopsis of each
 
+-- | Show help in the context of a Stack
 showHelp :: Stack -> IO ()
 showHelp s = showDescription s >> showExamples s
 
+-- | Show a description if one exists for the current Stack
 showDescription :: Stack -> IO ()
-showDescription (Stack (Kommand{_description=Just ls}:_) _) = do
+showDescription (Stack (K{_description=Just ls}:_) _) = do
   putStrLn "\nDescription:\n"
   mapM_ (putStrLn . (++) "  ") ls
   putStrLn "\n"
 showDescription _ = return ()
 
+-- | Show the examples if they exist for the current Stack
 showExamples :: Stack -> IO ()
-showExamples (Stack (Kommand{_examples=Just ls}:_) _) = do
+showExamples (Stack (K{_examples=Just ls}:_) _) = do
   putStrLn "\nExamples:\n"
   mapM_ (putStrLn . (++) "  ") ls
   putStrLn "\n"
@@ -177,4 +181,4 @@ main = do
   result <- kommands
   case result of -- JSON parse error
     Left  er -> hPutStrLn stderr er >> exitWith (ExitFailure 1)
-    Right ks -> route . initialStack . Stack ks =<< getArgs
+    Right ks -> run . initialStack . Stack ks =<< getArgs
