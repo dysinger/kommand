7bdb4b8fb8fc98333b59832036fbc151ed9e49c0
Tim Dysinger <tim@dysinger.net>
2013-05-21 15:55:11 -1000

    lunch time refactoring I did into dependent types

--- Main.hs
+++ Main.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE OverloadedStrings, RecordWildCards #-}
+{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE OverloadedStrings  #-}
+{-# LANGUAGE RankNTypes         #-}
+{-# LANGUAGE RecordWildCards    #-}
+{-# LANGUAGE TypeFamilies       #-}
 
 {-|
 Module      :  Main
@@ -24,6 +28,7 @@ import           Data.Aeson
 import qualified Data.ByteString.Lazy       as DBL
 import qualified Data.ByteString.Lazy.Char8 as DBLC
 import           Data.Char
+import           Data.List
 import           Data.Time.Clock
 import           Network.HTTP
 import           System.Cmd
@@ -56,12 +61,21 @@ instance FromJSON Kommand where
                          v .:? "url"
   parseJSON _          = mzero
 
--- | Printing a Kommand should show it's full path by default. If the
--- path isn't given then the kommand is a subcommand or flag.  In this
--- case we print it's id.
 instance Show Kommand where
-  show (K{_path=Nothing,_id=i}) = i
-  show (K{_path=Just p}) = p
+  show (K{_path=Nothing,_id=i}) = show i
+  show (K{_path=Just p})        = show p
+
+data family   Arg a
+data instance Arg Kommand = AK Kommand
+data instance Arg String  = AS String
+
+instance Show (Arg Kommand) where show (AK k) = show k
+instance Show (Arg String)  where show (AS s) = show s
+
+data Stack = Stack [Arg Kommand] [Arg String]
+
+instance Show Stack where
+  show (Stack ks as) = intercalate " " (map show ks ++ map show as)
 
 -- | Does this lowercased string match a lowercase Kommand's _id ?
 matchKommand :: String -> Kommand -> Bool
@@ -116,56 +130,60 @@ fetchKommands = do
   where
     req = getRequest "http://s3.amazonaws.com/knewton-public-src/kommands.json"
 
--- | Define a Stack of Kommands and Strings
-data Stack = Stack [Kommand] [String] deriving Show
-
--- | Build a new Stack containing the Kommands (in order), that
--- matched args in the list (in order), plus the remainder of the args
--- that didn't match.
-initialStack :: Stack -> Stack
-initialStack (Stack ks as) = Stack found (reverse left)
-  where (_, (Stack found left)) = foldl reduceArgs (Just ks, (Stack [] [])) as
+-- | Build a new Stack containing the Arg Kommands that matched Strings
+-- in the list (in order), plus the remainder of the Arg Strings that
+-- didn't match.
+initialStack :: [Kommand] -> [String] -> Stack
+initialStack ks as = Stack found (reverse left)
+  where (_, (Stack found left)) = foldl reduceAS (Just ks, (Stack [] [])) as
 
 -- | Take a tuple of maybe a list of Kommands and a String arg.  If
 -- it's a match to any of the Kommands in our list then put it on the
--- Kommand stack.  If not put it on the arg stack.
-reduceArgs :: (Maybe [Kommand], Stack) -> String -> (Maybe [Kommand], Stack)
-reduceArgs (x, Stack ks as) a =
+-- Arg Kommand stack.  If not put it on the Arg String stack.
+reduceAS :: (Maybe [Kommand], Stack) -> String -> (Maybe [Kommand], Stack)
+reduceAS (x, Stack ks as) a =
   case (filter (matchKommand a) internalKommands) of
-    (k:_) -> (x, Stack (k:ks) as)
+    (k:_) -> (x, Stack ((AK k):ks) as)
     _     -> case x of
-      Nothing -> (Nothing, Stack ks (a:as))
+      Nothing -> (Nothing, Stack ks ((AS a):as))
       Just y  -> case (filter (matchKommand a) y) of
-        (k:_) -> (_commands k, Stack (k:ks) as)
-        _     -> (Nothing, Stack ks (a:as))
+        (k:_) -> (_commands k, Stack ((AK k):ks) as)
+        _     -> (Nothing, Stack ks ((AS a):as))
 
 -- | Take a Stack and build a new Stack by replacing all help Kommands
 -- with a single help Kommand at the top of the stack (in case someone
 -- types `k -? -h help --help XYZ help`)
-helpStack :: Stack -> Stack
-helpStack s@(Stack ks as) =
-  let noHelp = filter ((/=) "help" . _id) ks in
+cleanHelpArgs :: Stack -> Stack
+cleanHelpArgs s@(Stack ks as) =
+  let noHelp = filter (\(AK k) -> "help" /= (_id k)) ks in
   if (length noHelp < length ks)
-     then (Stack (help:noHelp) as)
+     then (Stack ((AK help):noHelp) as)
      else s
 
 -- | Take a Stack and build a new Stack by popping Kommands onto the
 -- args list until we can find a Kommand that has an executable path.
 exeStack :: Stack -> Stack
 exeStack (Stack [] _) = error "Kommand didn't have an executable path."
-exeStack s@(Stack (K{_path=Just _}:_) _) = s
-exeStack (Stack (k@(K{_path=Nothing}):ks) as) =
-  exeStack (Stack ks ((show k):as))
+exeStack s@(Stack (AK K{_path=Just _}:_) _) = s
+exeStack (Stack (AK K{_path=Nothing,_id=i}:ks) as) =
+  exeStack (Stack ks ((AS i):as))
 
 -- | Run analyzes the stack & routes it to the appropriate function.
 run :: [Kommand] -> Stack -> IO ()
-run ks   (Stack [] [])                     = showKommands ks        >> putStr "\n"
-run _    (Stack [] (a:as))                 = exitWith =<< rawSystem a as
-run ks   (Stack ((K{_id="help"}):[]) _)    = showKommands ks        >> putStr "\n"
-run _    (Stack ((K{_id="help"}):ks) as)   = showHelp (Stack ks as) >> putStr "\n"
-run _  s@(Stack (K{_path=Nothing}:[]) _)   = showHelp s             >> putStr "\n"
-run ks s@(Stack (K{_path=Nothing}:_) _)    = run ks (exeStack s)
-run _    (Stack (k@K{_path=Just _}:_) as)  = exitWith =<< rawSystem (show k) as
+run ks   (Stack [] [])                         = listKommands ks      
+run _    (Stack [] (a:as))                     = runRawSystem a as
+run ks   (Stack (AK K{_id="help"}:[]) _)       = listKommands ks      
+run _    (Stack (AK K{_id="help"}:ks) as)      = showHelp (Stack ks as) 
+run _  s@(Stack (AK K{_path=Nothing}:[]) _)    = showHelp s     
+run ks s@(Stack (AK K{_path=Nothing}:_) _)     = run ks (exeStack s)
+run _    (Stack (k@(AK K{_path=Just _}):_) as) = runRawSystem k as
+
+runRawSystem :: forall a b c. Show a => Show b => a -> [b] -> IO c
+runRawSystem a as = return =<< exitWith =<< rawSystem (show a) (map show as)
+
+-- | List all the Kommands given
+listKommands :: [Kommand] -> IO ()
+listKommands ks = showKommands ks >> putStr "\n"
 
 -- | Show the list of Kommands & their synopsis - 1 per line
 showKommands :: [Kommand] -> IO ()
@@ -178,29 +196,27 @@ showKommands ks = do
 
 -- | Show help in the context of a Stack
 showHelp :: Stack -> IO ()
-showHelp s = do
-  showDescription s
-  showSubKommands s
-  showExamples s
+showHelp s =
+  showDescription s >> showSubKommands s >> showExamples s >> putStr "\n"
 
 -- | Show a description if one exists for the current Stack
 showDescription :: Stack -> IO ()
-showDescription (Stack (K{_description=Just ls}:_) _) = do
+showDescription (Stack (AK K{_description=Just ls}:_) _) = do
   putStrLn "\nDescription:\n"
   mapM_ (putStrLn . (++) "  ") ls
 showDescription _ = return ()
 
--- | Show the sub Kommands if they exist for the current Stack
-showSubKommands :: Stack -> IO ()
-showSubKommands (Stack (K{_commands=Just ls}:_) _) = showKommands ls
-showSubKommands _ = return ()
-
 -- | Show the examples if they exist for the current Stack
 showExamples :: Stack -> IO ()
-showExamples (Stack (K{_examples=Just ls}:_) _) = do
+showExamples (Stack (AK K{_examples=Just ls}:_) _) = do
   putStrLn "\nExamples:\n"
   mapM_ (putStrLn . (++) "  ") ls
 showExamples _ = return ()
+
+-- | Show the sub Kommands if they exist for the current Stack
+showSubKommands :: Stack -> IO ()
+showSubKommands (Stack (AK K{_commands=Just ls}:_) _) = showKommands ls
+showSubKommands _ = return ()
 
 -- | Main program entry point.
 main :: IO ()
@@ -208,4 +224,4 @@ main = do
   result <- kommands
   case result of -- JSON parse error
     Left  er -> hPutStrLn stderr er >> exitWith (ExitFailure 1)
-    Right ks -> run ks . helpStack . initialStack . Stack ks =<< getArgs
+    Right ks -> run ks . cleanHelpArgs . initialStack ks =<< getArgs
